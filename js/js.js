8ba56a4db3f3a8fe76d2e0e65aeb8e0a83d6037c
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-01-28 18:11:50 +0100

    started

---
+++ js/js.js
@@ -0,0 +1,18 @@
+// example of how var acts
+/*if(true){
+    var test = true;
+    }
+    alert(test);*/
+
+// example of how let acts
+/*if(true){
+    let test = true;
+    }
+    alert(test);
+*/
+// Objects 
+let profile = {
+name: 'mikkel',
+age: '24'
+};
+alert(profile.name);
