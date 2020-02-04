279ba783d7660ca6a2ce3f322f84f87db13cbead
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-02-04 22:29:16 +0100

    headless CMS movies started
    headless CMS movies started

--- day 6/headless_cms_template/js/main.js
+++ day 6/headless_cms_template/js/main.js
@@ -1,19 +1,101 @@
 "use strict";
-
-const url = "https://api.cederdorff.com/wp-json/wp/v2/posts";
-// const url = "https://api.cederdorff.com/wp-json/wp/v2/posts?_embed";
-
-fetch(url)
+// json
+const movies = "http://wordpress.beckmk.dk/wp-json/wp/v2/posts?_embed";
+const action = "http://wordpress.beckmk.dk/wp-json/wp/v2/posts?_embed&categories=21";
+const drama = "http://wordpress.beckmk.dk/wp-json/wp/v2/posts?_embed&categories=24";
+// fetch json
+function showAll(){
+fetch(movies)
   .then(function(response) {
     return response.json();
   })
   .then(function(posts) {
     console.log(posts);
+    appendPosts(posts)
   });
+};
+showAll();
 
+// action
+function genreAction(){
+fetch(action)
+.then(function(response){
+return response.json()
+})
+.then(function(genres){
+appendAction(genres)
+});
+};
+genreAction();
+
+function genreDrama(){
+  fetch(drama)
+  .then(function(response){
+  return response.json()
+  })
+  .then(function(genres){
+  appendDrama(genres)
+  });
+  };
+  genreDrama();
+
+
+// append posts
 function appendPosts(posts) {
-  // to do
+  let htmlTemplate ="";
+  for (let post of posts){
+  htmlTemplate += `
+  <article>
+  <img src="${getFeaturedImageUrl(post)}">
+  <h2>${post.title.rendered}</h2>
+  ${post.content.rendered}
+  </article>
+  `;
+  }
+document.querySelector("#movies").innerHTML = htmlTemplate;
+};
+
+// append action
+function appendAction(genres){
+let htmlTemplate ="";
+for(let genre of genres){
+htmlTemplate +=`
+<article>
+<h2>${genre.title.rendered}</h2>
+${genre.content.rendered}
+</article>
+`
 }
+document.querySelector("#actionSelect").innerHTML = htmlTemplate;
+};
+function appendDrama(genres){
+  let htmlTemplate ="";
+  for(let genre of genres){
+  htmlTemplate +=`
+  <article>
+  <h2>${genre.title.rendered}</h2>
+  ${genre.content.rendered}
+  </article>
+  `
+  }
+  document.querySelector("#dramaSelect").innerHTML = htmlTemplate;
+  };
+function hideAction(){
+let showAndHide = document.querySelector("#actionSelect");
+if (showAndHide.style.display ==="none"){
+showAndHide.style.display = "block";
+} else{
+showAndHide.style.display = "none";
+}
+};
+function hideDrama(){
+  let showAndHide = document.querySelector("#dramaSelect");
+  if (showAndHide.style.display ==="none"){
+  showAndHide.style.display = "block";
+  } else{
+  showAndHide.style.display = "none";
+  }
+  };
 
 // get the featured image url
 function getFeaturedImageUrl(post) {
@@ -22,4 +104,4 @@ function getFeaturedImageUrl(post) {
     imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
   }
   return imageUrl;
-}
+};
