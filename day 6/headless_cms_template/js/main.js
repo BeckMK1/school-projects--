f89994ff21763174da1e3b8d5a52f3e7e265880e
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-02-06 22:18:05 +0100

    HL CMS done
    can not load HL CMS

--- day 6/headless_cms_template/js/main.js
+++ day 6/headless_cms_template/js/main.js
@@ -1,19 +1,18 @@
 "use strict";
 // json
-const movies = "http://wordpress.beckmk.dk/wp-json/wp/v2/posts?_embed";
-const categories = "http://wordpress.beckmk.dk/wp-json/wp/v2/categories";
 
 let movies = [];
+
 // fetch json
 function showAll(){
-fetch(movies)
+fetch("http://wordpress.beckmk.dk/wp-json/wp/v2/posts?_embed")
   .then(function(response) {
     return response.json();
   })
   .then(function(json) {
     console.log(json);
     appendPosts(json);
-    movies = json
+    movies = json;
   });
 };
 showAll();
@@ -30,8 +29,9 @@ function appendPosts(movies) {
   </article>
   `;
   }
-document.querySelector("#movies").innerHTML = htmlTemplate;
+document.querySelector("#movies-container").innerHTML = htmlTemplate;
 };
+
 // search
 function search(value) {
   let searchQuery = value.toLowerCase();
@@ -47,28 +47,62 @@ function search(value) {
 };
 
 function getGenres(){
-fetch(categories)
+fetch("http://wordpress.beckmk.dk/wp-json/wp/v2/categories")
 .then(function(response){
  return response.json();
 })
 .then(function(categories){
   appendGenres(categories);
+  console.log(categories);
 });
 }
 getGenres();
+
 function appendGenres(genres){
 let htmlTemplate ="";
 for(let genre of genres){
 htmlTemplate +=`
 <option value="${genre.id}">${genre.name}</option>
-`;}
-document.querySelector("#se")
+`;
 }
-// get the featured image url
-function getFeaturedImageUrl(post) {
-  let imageUrl = "";
-  if (post._embedded['wp:featuredmedia']) {
-    imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
+document.querySelector("#select-genre").innerHTML += htmlTemplate;
+}
+function genreSelected(genreId){
+console.log(`genre ID: ${genreId}`);
+if (genreId){
+fetch(`http://wordpress.beckmk.dk/wp-json/wp/v2/posts?_embed&categories=${genreId}`)
+.then(function(response){
+  return response.json(); 
+})
+.then(function(movies) {
+console.log(movies);
+appendMoviesByGenre(movies);
+});
+} else {
+ document.querySelector("#movies-by-genre-container").innerHTML =`
+  <p>Please, select genre</p>
+ `; 
   }
-  return imageUrl;
-};
+}
+
+function appendMoviesByGenre(moviesByGenre){
+  let htmlTemplate = "";
+
+  for (let movie of moviesByGenre) {
+  htmlTemplate +=`
+  <article>
+  <h2>${movie.title.rendered} (${movie.acf.year})</h2>
+  <img src="${movie.acf.img}">
+  <p>${movie.acf.description}</p>
+  iframe src="${movie.acf.trailer}"></iframe>
+  </article>
+  `;
+  }
+  if (moviesByGenre.length === 0) {
+    htmlTemplate = `
+    <p> no movies</p>
+    `;
+  }
+document.querySelector("#movies-by-genre-container").innerHTML = htmlTemplate;
+}
+
